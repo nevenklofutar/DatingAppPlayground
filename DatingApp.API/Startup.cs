a9cc712d55171c339be2160473cbf7a8a11e8af4
unknown <neven.klofutar@gmail.com>
2019-05-21 23:11:19 +0200

    Added seed json file, added seed method as a service to be started on Startup.cs

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -35,11 +35,12 @@ namespace DatingApp.API
                 Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddCors();
+            services.AddTransient<Seed>();
             services.AddScoped<IAuthorizationRepository, AuthorizationRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
         {
             if (env.IsDevelopment())
             {
@@ -63,6 +64,7 @@ namespace DatingApp.API
             }
 
             // app.UseHttpsRedirection();
+            //seeder.SeedUsers(); // use it only once !!!
             app.UseCors(c => c.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
