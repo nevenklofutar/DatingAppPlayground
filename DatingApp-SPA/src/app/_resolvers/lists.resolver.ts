014db46ad435c4a2e75f04be15460580320d9eab
Neven Klofutar <neven.klofutar@gmail.com>
2019-07-03 20:05:02 +0200

    Added Like functionality

--- DatingApp-SPA/src/app/_resolvers/lists.resolver.ts
+++ DatingApp-SPA/src/app/_resolvers/lists.resolver.ts
@@ -9,14 +9,15 @@ import { catchError } from 'rxjs/operators';
 // getting data directly from the route so that component can't load without data
 
 @Injectable()
-export class MemberListResolver implements Resolve<User[]> {
+export class ListsResolver implements Resolve<User[]> {
      pageNumber = 1;
      pageSize = 5;
+     likesParams = 'Likers';
 
      constructor(private userService: UserService, private router: Router, private alertify: AlertifyService) {}
 
      resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
-          return this.userService.getUsers(this.pageNumber, this.pageSize).pipe(
+          return this.userService.getUsers(this.pageNumber, this.pageSize, null, this.likesParams).pipe(
                catchError(error => {
                     this.alertify.error('Problem retrieving data');
                     this.router.navigate(['/home']);
